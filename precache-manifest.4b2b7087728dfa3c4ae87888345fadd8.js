d5bea9ab9184e199a6c58b07d5c527d0c92c5d96
Andrew-Hany <andrewhany@aucegypt.edu>
2020-06-15 00:30:21 +0200

    Updates

---
+++ precache-manifest.4b2b7087728dfa3c4ae87888345fadd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab97797d237ba6036e9a971bc710336",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "c4d74e9c011e219fe329",
+    "url": "/To-Do-List/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "742879fd86d17369c134",
+    "url": "/To-Do-List/static/css/main.78f4f6a9.chunk.css"
+  },
+  {
+    "revision": "c4d74e9c011e219fe329",
+    "url": "/To-Do-List/static/js/2.7194f5d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.7194f5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742879fd86d17369c134",
+    "url": "/To-Do-List/static/js/main.90982b4f.chunk.js"
+  },
+  {
+    "revision": "39ae1b117d0b0560909a",
+    "url": "/To-Do-List/static/js/runtime-main.88408496.js"
+  },
+  {
+    "revision": "1e71c5ad8188e077f379d7964621a7e2",
+    "url": "/To-Do-List/static/media/bg.1e71c5ad.jpg"
+  }
+]);
